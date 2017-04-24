913292a7b80fdff9fb278dd30fb4b599abacc81f
Ben <bxc3201@rit.edu>
2017-04-24 14:51:55 -0400

    controller

--- client/controller.js
+++ client/controller.js
@@ -1,7 +1,10 @@
 "use strict";
 
 let socket;
+let nameEl;
+let roomEl;
 let messageEl;
+let loginContainer;
 let actionContainer;
 let voteContainer;
 let submitBtn;
@@ -11,37 +14,27 @@ const log = (output) => {
   console.log(output);
 }
 
-const initSockets = () => {
-    socket = io.connect();
+const q = (str) => {
+  return document.querySelector(str);
+}
 
-    socket.on("update", (data) => {
-        updateGameObject(data);
-    });
+const hideLogin = () => {
+  loginContainer.style.display = "none";
+}
 
-    socket.emit("joinGame");
-};
+const joinGame = () => {
+  log("asdF");
+  socket.emit("joinGame",{ "name": nameEl.innerHTML, "room": roomEl.innerHTML });
+  // temporary
+  messageEl.innerHTML = "You are not infected."
+  hideLogin();
+  displayPlayerChoices();
+}
 
-const initPage = () => {
-    document.onscroll = function(e){
-        e.preventDefault();
-    };
-    
-    // get references to DOM elements
-    messageEl = document.querySelector(".message");
-    actionContainer = document.querySelector(".action");
-    voteContainer = document.querySelector(".vote");
-    submitBtn = document.querySelector(".submit")
-    
-    // events
-    
-};
-
-// create a new choice button
-const createChoice = (choiceString) => {
-  const btn = document.create("button");
-  btn.innerHTML = choiceString;
-  activateChoiceBtn(btn);
-  actionContainer.appendChild(btn);
+// choice made
+const resolveChoice = () => {
+  // clear button continer
+  actionContainer.innerHTML = "";
 }
 
 // create a click listener for a choice button 
@@ -55,16 +48,53 @@ const activateChoiceBtn = (btn) => {
   return b;
 }
 
-// choice made
-const resolveChoice = () => {
-  // clear button continer
-  actionContainer.innerHTML = "";
+// create a new choice button
+const createChoice = (choiceString) => {
+  const btn = document.createElement("button");
+  btn.innerHTML = choiceString;
+  activateChoiceBtn(btn);
+  actionContainer.appendChild(btn);
 }
 
+
+// player choices on a given turn
+const displayPlayerChoices = () => {
+  createChoice("food");
+  createChoice("generator");
+  createChoice("chems");
+}
+
+const initSockets = () => {
+    socket = io.connect();
+
+    socket.on("update", (data) => {
+        updateGameObject(data);
+    });
+};
+
+const initPage = () => {
+    document.onscroll = function(e){
+        e.preventDefault();
+    };
+    
+    // get references to DOM elements
+    nameEl = q(".name");
+    roomEl = q(".room");
+    messageEl = q(".message");
+    loginContainer = q(".login");
+    actionContainer = q(".action");
+    voteContainer = q(".vote");
+    submitBtn = q(".submit");
+    log(submitBtn);
+    
+    // events
+    submitBtn.onclick = () => { joinGame(); };
+};
+
 const init = () => {
+  log("init");
   initSockets();
   initPage();
-
 };
 
 window.onload = init;
